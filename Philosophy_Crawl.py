b7433617c439c978e72536eda4e62bf16c12cb86
hliu26 <howardliu26@gmail.com>
2020-07-18 01:18:14 -0700

    updated functionality in removing parentheses, checking for tables, and gathering links

--- Philosophy_Crawl.py
+++ Philosophy_Crawl.py
@@ -6,6 +6,7 @@ import urllib
 from PIL import Image
 from io import BytesIO
 
+
 class Page:
     
     wiki = 'https://en.wikipedia.org'
@@ -24,6 +25,8 @@ class Page:
         except:
             self.image = None
             
+    def remove_table(self):
+        self.soup.find("table").extract()
     
     def remove_parentheses(self, body_text):
         """converts body of text from BeautifulSoup Object into String
@@ -32,30 +35,67 @@ class Page:
         """
         
         out = []
-        paren = False
+        paren = 0
+        italic =0
+
         for i in str(body_text).split(" "):
-    
-            if "(" in i and "href" not in i:
-                paren = True
+            #print(i)
+            if "(" in i :
+                if "href" not in i:
+                    for symbol in i:
+                        if symbol == "(":
+                            paren += 1
+                else:
+                    if paren != 0:
+                        paren += 1
+        
             if ")" in i:
-                paren = False
-    
-            if not paren:
-                if ")" not in i or "href" in i:
+                if "href" not in i:
+                    for symbol in i:
+                        if symbol == ")":
+                            paren -= 1
+                else:
+                    if paren != 0:
+                        paren -= 1
+                
+        
+            if "<i>" in i or "<i" in i:
+                if "<img" not in i:
+                    italic += 1
+        
+            if "</i>" in i:
+                if italic == 0:
+                    pass
+                else:
+                    italic -= 1
+        
+            #print(paren)    
+            #print(italic)
+            if paren == 0 and italic == 0:
+                if ")" in i:
+                    if "href" in i :
+                        out.append(i)
+                else:
                     out.append(i)
 
         return BeautifulSoup(' '.join(out))
             
-            
+        
     def all_links(self):
         """check_next takes a beautifulsoup object.
         returns all links found in bodies of text (tags "p")
         the result is a list wiki links in each body of text
         """
+        self.remove_table()
         
         res = []
         for tags in self.soup.find("body").find_all("p"):
             tags = self.remove_parentheses(tags)
+            
+            if (any(s in str(tags).split(' ')[0] for s in ["<p>\n<span", "<p><span"]) 
+                    or "text-align" in str(tags)):
+                continue
+            
             if tags.find("a") == None:
                 pass
             else:
@@ -68,6 +108,7 @@ class Page:
         return res
     
     
+
     def get_nexturl(self):
         for link in self.all_links():
             return self.wiki + link
@@ -104,4 +145,6 @@ class Page:
             
         self.branches += 1
         print("\033[1m Philosophy Reached! \033[0m", self.wiki+next_link)
+    
+
     
